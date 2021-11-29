71216b168a24b31297c70215f929c27b29be794e
tmulaka <tmulaka@users.noreply.github.com>
2021-11-29 18:45:42 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var name, city, state string
+
+func main() {
+	name, city, state = "TJ", "Plainsboro", "NJ"
+	fmt.Println("Nmae is ", name)
+	fmt.Println("City = ",  city)
+	fmt.Println("State is",  state)
+}
