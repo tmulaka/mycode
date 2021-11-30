6903874e7d2cc991bfd6b8de881ef087a7054d40
tmulaka <tmulaka@users.noreply.github.com>
2021-11-30 16:49:36 +0000

    working with methods

---
+++ methods02.go
@@ -0,0 +1,33 @@
+/* Alta3 Research | RZFeeser
+   Methods - value and pointer receivers   */
+
+
+package main
+ 
+import (
+    "fmt"
+)
+type Shark struct{
+    name string
+}
+
+// pass by value - makes a copy of what is passed (changes WILL NOT persist)
+func (a Shark)Swim(){
+    fmt.Println(a.name, "shark is swimming...")
+}
+
+// pass by pointer - do this when you need to make changes (changes WILL persist)
+func (a *Shark)SwimFaster(){
+    fmt.Println(a.name, "shark is swimming...")
+}
+ 
+func main() {
+    fish := Shark{"Tiger"}
+    
+    // both of these will work
+    fish.Swim()                // Tiger shark is running...
+    fmt.Println("no pointer ", fish.name)
+    fish.SwimFaster()          // Tiger shark is running...
+     fmt.Println("with pointer ", fish.name)
+}
+
