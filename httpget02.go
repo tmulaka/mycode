5bc56d57601731afa83bd572e9195726d467367b
tmulaka <tmulaka@users.noreply.github.com>
2021-12-02 22:25:43 +0000

    rest api, get & post

---
+++ httpget02.go
@@ -0,0 +1,36 @@
+/* RZFeeser | Alta3 Research
+   HTTP GET with io.Copy() */
+   
+package main
+
+import (
+    "io"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+
+    resp, err := http.Get("http://webcode.me")
+
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    defer resp.Body.Close()
+    
+    
+    /* The io.Copy() function copies from source to destination until
+       either EOF is reached on source or an error occurs. Returns the number of bytes
+       copied and the first error encountered while copying, if any.
+       here it self we are writing to Stdout, we no need to print again.
+       */
+    
+    _, err = io.Copy(os.Stdout, resp.Body)
+
+    if err != nil {
+      log.Fatal(err)
+    }
+}
+
